eb459882bbf1c29ea0e468534194fff0a0d5ca6b
Salomao S. <souza95salomao@gmail.com>
2026-05-13 12:42:42 +0000

    :card_file_box: update(db): adds missing registration modality repository

---
+++ src/main/java/com/salomovs/mastersys/repository/RegistrationModalityRepository.java
@@ -0,0 +1,8 @@
+package com.salomovs.mastersys.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.salomovs.mastersys.domain.RegistrationModality;
+
+public interface RegistrationModalityRepository
+extends JpaRepository<RegistrationModality, Long> {}
